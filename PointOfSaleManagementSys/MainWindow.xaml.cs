00877c4f8de1a0a708a007906589387c087d2f46
Eli Aman <e.amanollahi@gmail.com>
2017-04-12 14:17:50 -0400

    Changed Main Window Desing

--- PointOfSaleManagementSys/MainWindow.xaml.cs
+++ PointOfSaleManagementSys/MainWindow.xaml.cs
@@ -20,9 +20,62 @@ namespace PointOfSaleManagementSys
     /// </summary>
     public partial class MainWindow : Window
     {
+        string[,] items = new string[,]{
+                  {"Alexander keith ","Blond","BudWiser","Corona","lager","Staute"},
+                  {"Coffee","Green Tea","Tea", "Hot Chocolate","Cafe Latte","Cappuccino"}
+               ,{"Lasagna","Pasta","Pizza","Filet Mignon","Steak","Chicken Wing"},
+               {"Cheese Burger","General Tso","Koobide","Sushi","Ghormeh Sabzi","Poutine"},
+               {"Cheese Cake","Chocolate Cake","Tiramisu","Ice Cream Cake","Ginger Bread","Jelly"},
+               {"SYRAH","MERLOT","RIESLING","GEWÜRZTRAMINER","CHARDONNAY","PINOT NOIR"}
+        
+        
+        };
+
+       
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void ButtonBeer_Click(object sender, RoutedEventArgs e)
+        {
+            ItemList(0);
+        }
+        private void ButtonHotDrink_Click(object sender, RoutedEventArgs e)
+        {
+            ItemList(1);
+        }
+       
+        private void ButtonDinner_Click(object sender, RoutedEventArgs e)
+        {
+            ItemList(2);
+        }
+        private void ButtonLunch_Click(object sender, RoutedEventArgs e)
+        {
+            ItemList(3);
+        }
+       
+        private void ButtonDessert_Click(object sender, RoutedEventArgs e)
+        {
+            ItemList(4);
+        }
+        private void ButtonWine_Click(object sender, RoutedEventArgs e)
+        {
+            ItemList(5);
+        }
+        private void ItemList(int categoryId)
+        {
+            int id = categoryId;
+            List<string> itemList = new List<string>();
+            for (int i = 0; i < 6;  i++)
+            {
+                itemList.Add(items[id, i]);
+                
+               
+            }
+            LvItems.ItemsSource = itemList;
+           
         }
 
        
